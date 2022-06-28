4a8926a4c94c144392974999ef544f85b375af90
hseiji <henrique.hst@gmail.com>
2022-06-28 19:49:50 -0400

    add http_basic_auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  
   protect_from_forgery with: :exception
 
+
+  def authenticate
+    http_basic_authenticate_or_request_with name: ENV["HTTP_BASIC_AUTH_USER_ID"], password: ENV["HTTP_BASIC_AUTH_USER_PASSWORD"]
+  end
+  
+  
   private
+
 
   def cart
     @cart ||= cookies[:cart].present? ? JSON.parse(cookies[:cart]) : {}
