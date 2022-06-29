781fb58e5e027943c7ccb975afe5940ea795e4e3
hseiji <henrique.hst@gmail.com>
2022-06-28 22:22:26 -0400

    complete dashboard>show.erb

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,6 +3,7 @@ class Admin::DashboardController < ApplicationController
   before_action :authenticate
 
   def show
-   
+    @products = Product.all.count
+    @categories = Category.all.count   
   end
 end
